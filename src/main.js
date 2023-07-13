e6196f7c43999f0b38e183842048ae25318e8978
Sergej Burusas <Sergej_Burusas@mail.ru>
2023-07-13 20:54:30 +0300

    add files3

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import './main.scss';
+import "./container.scss";
+import './components/typography/typography.scss';
 import './components/navigation/navigation';
 import './components/header/header';
