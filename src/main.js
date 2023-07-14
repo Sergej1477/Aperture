2020cc2af08b6a971cd1ae04c57f7cd0c615aeca
Sergej Burusas <Sergej_Burusas@mail.ru>
2023-07-14 18:20:32 +0300

    add files4

--- src/main.js
+++ src/main.js
@@ -3,3 +3,6 @@ import "./container.scss";
 import './components/typography/typography.scss';
 import './components/navigation/navigation';
 import './components/header/header';
+import './components/button/button';
+import './components/promo/promo';
+import './components/japan/japan';
